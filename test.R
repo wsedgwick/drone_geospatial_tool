f91bf730f4382078d0d5ff377380baf38370e205
Wade Sedgwick <wsedgwick0@gmail.com>
2025-04-03 07:44:11 -0700

    compatibility with windows

--- test.R
+++ test.R
@@ -1,5 +1,15 @@
 library(exifr)
+# system.time({
+#   exifr::read_exif(list.files("D:\pix4d_processing\latam\chile\2025\peatland 1\20250227 peatland 1 rgb RX1\F1", full.names = TRUE)[1:50],
+#                    tags = c("FileName", "GPSLongitude", "GPSLatitude", "GPSAltitude", "DateTimeOriginal"))
+# })
+
+
+
+image_dir <- "D:/pix4d_processing/latam/chile/2025/peatland 1/20250227 peatland 1 rgb RX1/F1"
+image_files <- list.files(image_dir, full.names = TRUE)
+length(image_files)  # Check how many images it sees
+
 system.time({
-  exifr::read_exif(list.files('/Users/wsedgwick/Desktop/drone images for others/2025 Peru Flamingos/106FTASK', full.names = TRUE)[1:50],
-                   tags = c("FileName", "GPSLongitude", "GPSLatitude", "GPSAltitude", "DateTimeOriginal"))
+  exifr::read_exif(image_files[1:50], tags = c("FileName", "GPSLongitude", "GPSLatitude", "GPSAltitude", "DateTimeOriginal"))
 })
